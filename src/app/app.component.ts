532791de1c0d23335bd31e5ca2d49b039eb16711
@ismaelnv <jonasvillarruelnegrete@gmail.com>
2024-06-18 04:56:30 -0500

    Lista de tareas en a ngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ListaTareasApp';
+export class AppComponent implements OnInit {
+ 
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  private _serviceTarea = inject(TareasService);
+
+  ngOnInit(): void {
+    
+    this.listaTareas = this._serviceTarea.getTareas();
+  }
+
+  agregarTarea(){
+
+    this._serviceTarea.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._serviceTarea.getTareas();
+  }
+
+  eliminarTarea(index:number){
+
+    this._serviceTarea.eliminarTarea(index);
+    this.listaTareas = this._serviceTarea.getTareas();
+  }
+
+
 }
